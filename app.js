0a0dc6737a9f780c8a20e84ee47eba4c4e15a543
AzureAD\박인태 <pit1995@outlook.kr>
2024-05-18 01:52:13 +0900

    const and let

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const a = 5;
-const b = 2;
+let a = 5;
+let b = 2;
 const myName = "nico";
-
-console.log ("a * b");
-console.log("a + b");
-console.log("a / b");
+//let: 변수 ,const: 상수
+//const를 기본으로 사용 -> 대부분의 경우 변수의 값을 고정하는 경우가 많기 때문
+console.log (a * b);
+console.log(a + b);
+console.log(a / b);
 console.log("hello" + myName);
+
+a = 3;
+b = 5;
+myName = "cheez";
+
+console.log (a * b);
+console.log(a + b);
+console.log(a / b);
+console.log("new name" + myName); // error 
