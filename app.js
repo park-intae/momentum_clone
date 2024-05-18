3441341e0563b6ee612f267a6585cd700b6756d9
AzureAD\박인태 <pit1995@outlook.kr>
2024-05-18 16:41:41 +0900

    array

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const amIFat = false;
-// boolean = true or false
-// null = nothing value is  in it
-// undefine = empty
-let something;
-console.log(something, amIFat);
+const daysOfWeek = [mon, tue, wed, thu, fri, sat];
+// array = 배열, 안에 어떤 타입의 값이든 들어갈 수 있음, 대괄호로 표시
+// 배열을 셀때는 0부터
+console.log(daysOfWeek);
+console.log(daysOfWeek[5]);
+// 배열의 어떤 위치(index)의 값을 불러올 때는 그 위치를 대괄호 안에
+// 배열에 요소 추가하기
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
