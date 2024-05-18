c7c4853a96a605aaa2b7e645dfd7f4936a3aeb19
AzureAD\박인태 <pit1995@outlook.kr>
2024-05-18 15:57:49 +0900

    boolean, null, undefine

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
-let a = 5;
-let b = 2;
-const myName = "nico";
-//let: 변수 ,const: 상수
-//const를 기본으로 사용 -> 대부분의 경우 변수의 값을 고정하는 경우가 많기 때문
-console.log (a * b);
-console.log(a + b);
-console.log(a / b);
-console.log("hello" + myName);
-
-a = 3;
-b = 5;
-myName = "cheez";
-
-console.log (a * b);
-console.log(a + b);
-console.log(a / b);
-console.log("new name" + myName); // error 
+const amIFat = false;
+// boolean = true or false
+// null = nothing value is  in it
+// undefine = empty
+let something;
+console.log(something, amIFat);
