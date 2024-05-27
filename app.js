63ce04e1320da5d20a6e0f9d1b62d1976c28b52f
AzureAD\박인태 <pit1995@outlook.kr>
2024-05-27 20:06:22 +0900

    CSS in js

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    console.log("titile was clicked!");
-    title.style.color = "blue";
-}
-function handleTitleMouseenter(){
-    title.innerText = "Mouse is here!";
-}
-function handleTitleMouseleave(){
-    title.innerText = "Mouse is gone!"
-}
-
-title.addEventListener("click",handleTitleClick);
-// = title.onclick = handleTitleClick;
-title.addEventListener("mouseenter", handleTitleMouseenter);
-title.addEventListener("mouseleave", handleTitleMouseleave);
-
-function handelWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-function handelWindowCopy(){
-    alert("copy hasbeen");
+    const curColor = h1.style.color;
+    let newColor = h1.style.color;
+    if(curColor === "blue"){
+        newColor = "tomato"
+    }else{
+        newColor = "blue"
+    }
+    h1.style.color = newColor;
 }
 
-window.addEventListener("resize", handelWindowResize);
-window.addEventListener("copy", handelWindowCopy);
+h1.addEventListener("click",handleTitleClick);
