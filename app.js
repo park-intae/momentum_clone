de8760d55f2ef1df2f5c794d0dfd7f2d9906fe37
AzureAD\박인태 <pit1995@outlook.kr>
2024-05-27 18:34:05 +0900

    event part2

--- app.js
+++ app.js
@@ -4,5 +4,13 @@ function handleTitleClick(){
     console.log("titile was clicked!");
     title.style.color = "blue";
 }
+function handleTitleMouseenter(){
+    title.innerText = "Mouse is here!";
+}
+function handleTitleMouseleave(){
+    title.innerText = "Mouse is gone!"
+}
 
 title.addEventListener("click",handleTitleClick);
+title.addEventListener("mouseenter", handleTitleMouseenter);
+title.addEventListener("mouseleave", handleTitleMouseleave);
