da031d0deeac665cb62bf797bd084920eabe9837
AzureAD\박인태 <pit1995@outlook.kr>
2024-05-27 19:02:07 +0900

    more events

--- app.js
+++ app.js
@@ -12,5 +12,16 @@ function handleTitleMouseleave(){
 }
 
 title.addEventListener("click",handleTitleClick);
+// = title.onclick = handleTitleClick;
 title.addEventListener("mouseenter", handleTitleMouseenter);
 title.addEventListener("mouseleave", handleTitleMouseleave);
+
+function handelWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+function handelWindowCopy(){
+    alert("copy hasbeen");
+}
+
+window.addEventListener("resize", handelWindowResize);
+window.addEventListener("copy", handelWindowCopy);
