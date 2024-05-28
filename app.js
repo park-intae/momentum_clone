78996a9ceb5a8d49ab718d9c1c24326f6cabc694
AzureAD\박인태 <pit1995@outlook.kr>
2024-05-28 17:57:19 +0900

    CSS in JS part3-1 function으로 class 확인/추가/제거

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
     const clickedClass = "clicked"
-    if (h1.className === clickedClass){
-        h1.className = "";
+    if (h1.classList.contains(clickedClass)){ // classList에"clikedClass"클래스가 포함되어 있는지 확인
+        h1.classList.remove(clickedClass); // classList에서 "clikedclass"클래스 제거
     }else{
-        h1.className = clickedClass;
+        h1.classList.add(clickedClass);
     }
 }
 
