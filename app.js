8dfbd3923c78f7f922c13cac74e9e1321e11053f
AzureAD\박인태 <pit1995@outlook.kr>
2024-05-28 19:44:51 +0900

    CSS in JS part3-2 toggle을 통해 코드 단축화

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
     const clickedClass = "clicked"
-    if (h1.classList.contains(clickedClass)){ // classList에"clikedClass"클래스가 포함되어 있는지 확인
-        h1.classList.remove(clickedClass); // classList에서 "clikedclass"클래스 제거
-    }else{
-        h1.classList.add(clickedClass);
-    }
+    // if (h1.classList.contains(clickedClass)){ // classList에"clikedClass"클래스가 포함되어 있는지 확인
+    //     h1.classList.remove(clickedClass); // classList에서 "clikedclass"클래스 제거
+    // }else{
+    //     h1.classList.add(clickedClass);
+    // }
+    //아래 코드 한줄로 대체 가능
+
+    h1.classList.toggle(clickedClass);
 }
 
 h1.addEventListener("click",handleTitleClick);
