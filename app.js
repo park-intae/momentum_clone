e34ff64ff3c7486b50c84c1bbde76692fa0bdca3
AzureAD\박인태 <pit1995@outlook.kr>
2024-06-01 16:56:32 +0900

    Loading Username

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const loginForm = document.querySelector("#login-form");
-const loninInput = document.querySelector("#login-form input");
+const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNA);
-    const username = loninInput.value;
-    localStorage.setItem("username", username);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(){
     greeting.classList.remove(HIDDEN_CLASSNA);
     greeting.innerText = `Hello ${username}`;
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
 link.addEventListener("click", handleLinkClick);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(saveUsername === null){
+    //show form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else {
+    //show greetings
+    paintGreetings(savedUsername);
+}
